1290d33eaa7c2388d335eaec8ec8e0f31d4ac8a9
Andrew <macarieandrei8@gmail.com>
2025-02-22 12:14:55 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+time.sleep(2)
+# print(pyautogui.position())
+pyautogui.moveTo(700,400,2)
+pyautogui.mouseDown()
+pyautogui.moveTo(700,800,2)
+pyautogui.moveTo(400,-800,2)
+pyautogui.moveTo(400,-400,2)
+# pyautogui.moveTo(,500,2)
+# pyautogui.mouseUp()
